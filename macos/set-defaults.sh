406a173de958beb5b2d1a5d21a50a8553941ba91
Berzan Yildiz <berzanmikaili@gmail.com>
2020-05-21 01:47:45 +0200

    Font rendering on external monitor

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -8,7 +8,7 @@
 # Run ./set-defaults.sh and you'll be good to go.
 
 # Fixes thin fonts on iterm with external monitors
-defaults -currentHost write -globalDomain AppleFontSmoothing -int 2
+defaults write -g CGFontRenderingFontSmoothingDisabled -bool NO
 
 # Disable press-and-hold for keys in favor of key repeat.
 defaults write -g ApplePressAndHoldEnabled -bool false
