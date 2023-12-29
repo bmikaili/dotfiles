25a997d69b7c1b3b4482a4ad0017afded8d849e3
Berzan Yildiz <17531789+bmikaili@users.noreply.github.com>
2023-12-29 23:28:40 +0100

    add smart splits

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,7 +2,20 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
-vim.keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>", { silent = true })
-vim.keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>", { silent = true })
-vim.keymap.set("n", "<C-h>", ":TmuxNavigateLeft<CR>", { silent = true })
-vim.keymap.set("n", "<C-l>", ":TmuxNavigateRight<CR>", { silent = true })
+-- resizing splits
+-- these keymaps will also accept a range,
+-- for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
+vim.keymap.set("n", "<C-w>h", require("smart-splits").resize_left, { remap = true })
+vim.keymap.set("n", "<C-w>j", require("smart-splits").resize_down, { remap = true })
+vim.keymap.set("n", "<C-w>k", require("smart-splits").resize_up, { remap = true })
+vim.keymap.set("n", "<C-w>l", require("smart-splits").resize_right, { remap = true })
+-- moving between splits
+vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
+vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
+vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
+vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
+-- swapping buffers between windows
+vim.keymap.set("n", "<leader><leader>h", require("smart-splits").swap_buf_left)
+vim.keymap.set("n", "<leader><leader>j", require("smart-splits").swap_buf_down)
+vim.keymap.set("n", "<leader><leader>k", require("smart-splits").swap_buf_up)
+vim.keymap.set("n", "<leader><leader>l", require("smart-splits").swap_buf_right)
